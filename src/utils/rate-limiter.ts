f7dbdd94ba7db32d1dd803f2c6df6eb82073675b
Sunny <swarnendughosh05@gmail.com>
2025-04-03 00:21:55 +0530

    update

--- src/utils/rate-limiter.ts
+++ src/utils/rate-limiter.ts
@@ -1,10 +1,9 @@
 import { rateLimit } from 'express-rate-limit'
 
 export const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-	// store: ... , // Redis, Memcached, etc. See below.
+	windowMs: 15 * 60 * 1000, 
+	limit: 100, 
+	standardHeaders: 'draft-8',
+	legacyHeaders: false, 
 })
 
