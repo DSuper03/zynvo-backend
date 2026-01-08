a111d0fb1acc1aa8ddec3f8ebf8b05cc8d4956d1
Rishiraj <rishirajnatj@gmail.com>
2026-01-08 21:15:57 +0530

    Auth fix

--- src/routes/eventRouter.ts
+++ src/routes/eventRouter.ts
@@ -40,12 +40,12 @@ router.get('/ver-event', verifyEventRegistration);
 router.get('/event-details', getEventDetails);
 router.get('/participants/:eventId', AuthMiddleware, AdminCoreAuthMiddleware, eventAttendees);
 router.get('/getAnn/:eventId',AuthMiddleware, getAllannouncements);
-router.post('/announcement/:eventId',AuthMiddleware, ClubHeadAuthMiddleware, createAnnouncement);
-router.put('/announcement/:eventId',AuthMiddleware, ClubHeadAuthMiddleware, updateAnnouncement);
-router.delete('/announcement/:eventId',AuthMiddleware, ClubHeadAuthMiddleware, deleteAnnouncement);
+router.post('/announcement/:eventId',AuthMiddleware, SpecificClubHeadAuthMiddleware, createAnnouncement);
+router.put('/announcement/:eventId',AuthMiddleware, SpecificClubHeadAuthMiddleware, updateAnnouncement);
+router.delete('/announcement/:eventId',AuthMiddleware, SpecificClubHeadAuthMiddleware, deleteAnnouncement);
 router.post('/:eventId/gallery', SpecificClubHeadAuthMiddleware, addToGallery);
 router.get('/:eventId/gallery', getEventGallery);
-router.put('/:eventId/gallery',AuthMiddleware, ClubHeadAuthMiddleware, updateGalleryItem);
-router.delete('/:eventId/gallery',AuthMiddleware, ClubHeadAuthMiddleware, deleteGalleryItem);
+router.put('/:eventId/gallery',AuthMiddleware, SpecificClubHeadAuthMiddleware, updateGalleryItem);
+router.delete('/:eventId/gallery',AuthMiddleware, SpecificClubHeadAuthMiddleware, deleteGalleryItem);
 
 export const EventRouter = router;
