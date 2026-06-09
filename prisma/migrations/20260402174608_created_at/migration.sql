4c1c6e44fe7910bf973df829679a91ebcd8c9098
Rishiraj <rishirajnatj@gmail.com>
2026-06-09 22:05:05 +0530

    feat: add EventCustomQuestion and RegistrationAnswer tables with foreign key constraints

--- prisma/migrations/20260402174608_created_at/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "clubs" ADD COLUMN "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
-
--- AlterTable
-ALTER TABLE "event" ADD COLUMN "createdById" TEXT;
-
--- AddForeignKey
-ALTER TABLE "event" ADD CONSTRAINT "event_createdById_fkey" FOREIGN KEY ("createdById") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
